d7647222f791cffbc58d6c9154325d8fe8ba47cf
sefaapinar <sefanur.pinar@gmail.com>
2021-10-31 17:39:10 +0300

    Initial commit

---
+++ src/Introduction/Main.java
@@ -0,0 +1,19 @@
+package Introduction;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int a,b;
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("A sayısını Giriniz:...");
+        a = input.nextInt();
+
+        System.out.println("B sayısını Giriniz:...");
+        b = input.nextInt();
+
+        System.out.println("A sayısı: " + a);
+        System.out.println("B sayısı: " + b);
+
+    }
+}
