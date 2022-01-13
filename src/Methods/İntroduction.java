5171d2201d77a5fee87346116c881ee51eb7530d
sefaapinar <sefanur.pinar@gmail.com>
2022-01-13 20:33:43 +0300

    Create İntroduction.java

---
+++ src/Methods/İntroduction.java
@@ -0,0 +1,10 @@
+package Methods;
+import java.util.Scanner;
+public class İntroduction {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+    }
+
+}
