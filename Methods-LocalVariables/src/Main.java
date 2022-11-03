a4029e0791d38d0fe7f2dc545878f96e4f48579a
sefaapinar <sefanur.pinar@gmail.com>
2022-11-03 22:14:45 +0300

    Methods-Local Variable

---
+++ Methods-LocalVariables/src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+
+    static void sum(int a, int b){
+        a = 24;
+        System.out.println(a);
+    }
+    public static void main(String[] args) {
+
+//        int a = 5;
+//        if(a<10){
+//            int b = 20;
+//        }
+//
+//        System.out.println(b);
+
+
+        int a = 20;
+        int b = 10;
+    }
+}
