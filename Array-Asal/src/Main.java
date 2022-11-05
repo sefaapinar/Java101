bcb3b17f8bc0593ddf94c135f6b232ea28cd92db
sefaapinar <77726883+sefaapinar@users.noreply.github.com>
2022-11-06 01:11:14 +0300

    AsalArray

---
+++ Array-Asal/src/Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("--------------------------------");
+        for(int number=2;number<100;number++){
+            int control=0;
+            for (int i=2;i<number;i++){
+                if (number%i==0){
+                    control=1;
+                    break;
+                }
+            }
+            if (control==0){
+                System.out.print(number+" ");
+            }
+        }
+
+        System.out.print("\n--------------------------------");
+    }
+}
