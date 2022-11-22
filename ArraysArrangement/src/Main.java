3ed360a3c4a08afcf464dc8ec41052d0985c109b
sefaapinar <77726883+sefaapinar@users.noreply.github.com>
2022-11-22 20:41:46 +0300

    ArraysArrangement

--- ArraysArrangement/src/Main.java
+++ ArraysArrangement/src/Main.java
@@ -1,5 +1,21 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Scanner number = new Scanner(System.in);
+        System.out.println("Enter the please number");
+        int size = number.nextInt();
+        int list[] = new int[size];
+
+
+        for(int i = 0; i<list.length; i++){
+            System.out.println((i+1)+". number: ");
+            list[i] = number.nextInt();
+
+        }
+        Arrays.sort(list);
+        System.out.println(Arrays.toString(list));
     }
 }
