c7858be5caf795c901ed8fe640aa948415c9b83c
sefaapinar <77726883+sefaapinar@users.noreply.github.com>
2022-11-23 21:45:56 +0300

    MathsClassAndMethods
    MathsClassAndMethods

---
+++ MathClassAndMethods/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println(Math.abs(-100)); //mutlak değere bakar.
+        System.out.println(Math.cbrt(8));  //Küp kökünü bulur.
+        System.out.println(Math.ceil(1.12)); //en yakın tam sayıya yuvarlanmış x değerini bizlere verir.
+        System.out.println(Math.floor(1.0001)); //en yakın tam sayıya yuvarlanmış x değerini bizlere verir.
+
+        
+
+    }
+}
